5df85e955a5943db327531ac12721da56e17bc2d
Adrian Nagy <adrian.nagy@simlestaking.com>
2019-06-25 10:40:21 +0200

    add note on slip 44 value

--- core/src/apps/libra/__init__.py
+++ core/src/apps/libra/__init__.py
@@ -7,6 +7,7 @@ CURVE = "ed25519"
 
 
 def boot():
+    # note: the value '55555' is arbitrary, we should use the slip 44 value (not yet registered)
     ns = [[CURVE, HARDENED | 44, HARDENED | 55555]]
     wire.add(MessageType.LibraGetAddress, __name__, "get_address", ns)
     wire.add(MessageType.LibraGetPublicKey, __name__, "get_public_key", ns)
