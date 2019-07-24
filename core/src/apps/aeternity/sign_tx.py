627d69d2a03a35a210401af6a615a63cd25e5dfc
Adrian Nagy <adrian.nagy@simplestaking.com>
2019-07-24 13:17:36 +0200

    WIP: prepare message handling

--- core/src/apps/aeternity/sign_tx.py
+++ core/src/apps/aeternity/sign_tx.py
@@ -19,8 +19,49 @@ async def sign_tx(ctx, msg, keychain):
 
     node = keychain.derive(msg.address_n, CURVE)
 
-    await layout.require_confirm_tx(ctx, msg.recipient_id, msg.amount)
-    await layout.require_confirm_fee(ctx, msg.amount, msg.fee)
+    if msg.spend is not None:
+        await layout.require_confirm_tx(ctx, msg.recipient_id, msg.amount)
+        await layout.require_confirm_fee(ctx, msg.amount, msg.fee)
+    elif msg.aens_preclaim is not None:
+        pass
+    elif msg.aens_claim is not None:
+        pass
+    elif msg.aens_update is not None:
+        pass
+    elif msg.aens_transfer is not None:
+        pass
+    elif msg.aens_revoke is not None:
+        pass
+    elif msg.contract_create is not None:
+        pass
+    elif msg.contract_call is not None:
+        pass
+    elif msg.channel_create is not None:
+        pass
+    elif msg.channel_deposit is not None:
+        pass
+    elif msg.channel_withdraw is not None:
+        pass
+    elif msg.channel_close_mutual is not None:
+        pass
+    elif msg.channel_close_solo is not None:
+        pass
+    elif msg.channel_slash is not None:
+        pass
+    elif msg.channel_settle is not None:
+        pass
+    elif msg.channel_snapshot is not None:
+        pass
+    elif msg.channel_force_progress is not None:
+        pass
+    elif msg.oracle_register is not None:
+        pass
+    elif msg.oracle_query is not None:
+        pass
+    elif msg.oracle_response is not None:
+        pass
+    elif msg.oracle_extend is not None:
+        pass
 
     if msg.network == AeternityNetworkType.MainNet:
         network_id = helpers.AETERNITY_NETWORK_ID_MAINNET.encode('utf-8')
@@ -56,38 +97,53 @@ async def sign_tx(ctx, msg, keychain):
 
 
 def encode_transaction(msg):
-    payload_bytes = bytearray()
-    write_bytes(payload_bytes, bytes(msg.payload.encode("utf-8")))
+    if msg.spend is not None:
+        payload_bytes = bytearray()
+        write_bytes(payload_bytes, bytes(_encode_string(msg.payload)))
 
-    tx_fields = [
-        _encode_int(helpers.AETERNITY_OBJECT_TAG_SPEND_TRANSACTION),
-        _encode_int(helpers.AETERNITY_VSN),
-        _encode_id(
-            helpers.base58_decode_check_prepend(
-                msg.sender_id, prefix=helpers.AETERNITY_TRANSACTION_SIGNATURE_PREFIX
-            )
-        ),
-        _encode_id(
-            helpers.base58_decode_check_prepend(
-                msg.recipient_id, prefix=helpers.AETERNITY_TRANSACTION_SIGNATURE_PREFIX
-            )
-        ),
-        _encode_int(msg.amount),
-        _encode_int(msg.fee),
-        _encode_int(msg.ttl),
-        _encode_int(msg.nonce),
-        payload_bytes,
-    ]
+        tx_fields = [
+            _encode_int(helpers.AETERNITY_OBJECT_TAG_SPEND_TRANSACTION),
+            _encode_int(helpers.AETERNITY_VSN),
+            _encode_id(
+                msg.sender_id, helpers.AETERNITY_TRANSACTION_SIGNATURE_PREFIX
+            ),
+            _encode_id(
+                msg.recipient_id, helpers.AETERNITY_TRANSACTION_SIGNATURE_PREFIX
+            ),
+            _encode_int(msg.amount),
+            _encode_int(msg.fee),
+            _encode_int(msg.ttl),
+            _encode_int(msg.nonce),
+            payload_bytes,
+        ]
+    elif msg.aens_preclaim is not None:
+        tx_fields = [
+            _encode_int(helpers.AETERNITY_OBJECT_TAG_NAME_SERVICE_PRECLAIM_TRANSACTION),
+            _encode_int(helpers.AETERNITY_VSN),
+            _encode_id(msg.aens_preclaim.account_id, helpers.AETERNITY_OBJECT_TAG_NAME_SERVICE_PRECLAIM_TRANSACTION),
+            _encode_int(msg.nonce),
+            _encode_id(msg.aens_preclaim.commitment_id, )
+        ]
 
     return rlp.encode(tx_fields)
 
 
+# TODO: implement the _binary() encoding method from aepp-sdk
+def _encode_string(str):
+    return str.encode('utf-8')
+
+
 def _encode_int(val: int):
-    return write_uint_arbitrary_be(val)
+    return write_uint_minimum_be(val)
 
 
-def _encode_id(id_str):
-    w = write_uint_arbitrary_be(1)
+def _encode_id(id_str, prefix):
+    w = write_uint_minimum_be(1)
+
+    helpers.base58_decode_check_prepend(
+        id_str, prefix=prefix
+    )
+
     write_bytes(w, id_str)
     return w
 
@@ -105,7 +161,7 @@ def get_byte_count(val: int):
         return count
 
 
-def write_uint_arbitrary_be(n: int):
+def write_uint_minimum_be(n: int):
     w = bytearray()
     byte_count = get_byte_count(n)
 
---
+++ python/trezorlib/tests/device_tests/ae_tran_test.py
@@ -0,0 +1,75 @@
+import namedtupled
+from aeternity import identifiers, node, signing, transactions
+from aeternity.hashing import _binary, decode, encode, encode_rlp
+from trezorlib import aeternity, messages, ui
+from trezorlib.client import TrezorClient
+from trezorlib.protobuf import dict_to_proto
+from trezorlib.tools import parse_path
+from trezorlib.transport import get_transport
+
+
+def trezor_connect():
+    transport = get_transport()
+    client = TrezorClient(transport, ui=ui.ClickUI())
+
+    return client
+
+
+client = trezor_connect()
+
+aeternity_cli = node.NodeClient(
+    node.Config(external_url="https://sdk-testnet.aepps.com")
+)
+
+path = parse_path("m/44'/457'/0'/0'/23'")
+
+nonce = aeternity_cli.get_next_nonce(
+    "ak_2Hdu87W2vkSxy6HXazvXnsnUG2GRP8bBY2jQQQuodfpKudKWKW"
+)
+
+# generate transaction object
+tx_builder = transactions.TxBuilder()
+txn = tx_builder.tx_spend(
+    "ak_2Hdu87W2vkSxy6HXazvXnsnUG2GRP8bBY2jQQQuodfpKudKWKW",
+    "ak_keR2L974TBQGS3RNq4Diz6poPo5ce2UmPQz5wrK25jZmmmBo3",
+    1000000000000000,
+    "txn with tT",
+    0,  # fee, when 0 it is automatically computed
+    0,  # ttl for the transaction in number of blocks (default 0)
+    nonce,
+)
+
+# transaction objet -> protobuf messsage
+trans = dict_to_proto(
+    messages.AeternitySignTx,
+    {
+        "network": messages.AeternityNetworkType.TestNet,
+        "sender_id": txn.data.sender_id,
+        "recipient_id": txn.data.recipient_id,
+        "amount": txn.data.amount,
+        "fee": txn.data.fee,
+        "ttl": txn.data.ttl,
+        "nonce": txn.data.nonce,
+        "payload": txn.data.payload,
+    },
+)
+
+# sign with trezor
+signed_tx = aeternity.sign_tx(client, path, trans)
+
+print(signed_tx.raw_bytes.hex())
+print(signed_tx.signature)
+print(signed_tx.tx_hash)
+print(encode(identifiers.TRANSACTION, signed_tx.raw_encoded_tx))
+
+# encode(prefix + base64encode) the transaction data with included signature
+aeternity_cli.broadcast_transaction(
+    encode(identifiers.TRANSACTION, signed_tx.raw_encoded_tx), signed_tx.tx_hash
+)
+
+print(f"https://testnet.explorer.aepps.com/#/tx/{signed_tx.tx_hash}")
+print(f"now waiting for confirmation (it will take ~9 minutes)...")
+
+# aeternity_cli.wait_for_confirmation(signed_tx.tx_hash)
+
+print(f"transaction confirmed!")
