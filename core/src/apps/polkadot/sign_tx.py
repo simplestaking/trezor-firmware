986de1759af8ab912fe29ee8d3fb5e99966a8398
Adrian Nagy <adrian.nagy@simplestaking.com>
2019-08-19 14:47:40 +0200

    signature accepted on polkadot network (alexander)

--- core/src/apps/polkadot/sign_tx.py
+++ core/src/apps/polkadot/sign_tx.py
@@ -9,27 +9,34 @@ from apps.common import paths
 from apps.common.writers import write_bytes, write_uint8, write_uint64_le, write_uint128_le
 from apps.polkadot import helpers, CURVE
 from ubinascii import hexlify
-
+from trezor.crypto import bip39
 
 async def sign_tx(ctx, msg, keychain):
 
     node = keychain.derive(msg.address_n, CURVE)
 
+    seed = bip39.seed('all all all all all all all all all all all all', "m'/357'/0'")
+    print('SEED: ' + hexlify(seed).decode())
+
     w = bytearray()
 
     # debug
     z = bytearray()
-    helpers.scale_int_encode(z, 6)
+    helpers.scale_int_encode(z, 142)
     print('Nonce: ' + hexlify(z).decode())
 
+    method_len = len(msg.transfer.destination) + 3 + helpers.get_byte_count(msg.transfer.value)
+
     # signing for Alexander network
+    helpers.scale_int_encode(w, msg.nonce)
+    # helpers.scale_int_encode(w, method_len)
     write_uint8(w, msg.transfer.module_index)
     write_uint8(w, msg.transfer.call_index)
     write_bytes(w, helpers.scale_encode_address(msg.transfer.destination))
     helpers.scale_int_encode(w, msg.transfer.value)
     write_bytes(w, msg.era)
-    helpers.scale_int_encode(w, msg.nonce)
-    helpers.scale_int_encode(w, msg.tip)
+
+    # helpers.scale_int_encode(w, msg.tip)
 
     # immortal
     if msg.genesis_hash:
@@ -41,7 +48,8 @@ async def sign_tx(ctx, msg, keychain):
     signature = ed25519.sign(node.private_key(), w)
     hex_sig = hexlify(signature).decode()
 
-    hex_data = hexlify(w)
-    print(hex_data)
+    hex_data = hexlify(w).decode()
+    print('SIGNED DATA: ' + hex_data)
 
     return PolkadotSignedTx(signature=hex_sig)
+
--- python/trezorlib/tests/device_tests/polkadot_sign_tx.py
+++ python/trezorlib/tests/device_tests/polkadot_sign_tx.py
@@ -17,9 +17,10 @@ print(polkadot.get_address(client, path))
 
 msg = dict_to_proto(messages.PolkadotSignTx, {
     "era": "00",
-    "nonce": 0x04,
+    "nonce": 0x00,
     "tip": 0x00,
     "genesis_hash": "dcd1346701ca8396496e52aa2785b1748deb6db09551b72159dcb3e08991025b",
+    # "block_hash": "9b0e498c1e77204f7718ac7d8efa6b89ba550f1bbd0915e09536641ff40b219e",
     "transfer": {
         "module_index": 0x03,
         "call_index": 0x00,
@@ -31,7 +32,3 @@ msg = dict_to_proto(messages.PolkadotSignTx, {
 signed = polkadot.sign_tx(client, path, msg)
 
 print(signed.signature)
-
-# 0x3d02    81  ffddd5ccff603c3801324a92fdb94d160d5efe5c460c491e3aed9117a1b340e69e  b818cbb3703a95fbd32de93d7de4413ff9b4d3c21b8ef55912092cd9e9a2500477cc611ca48c0e054791897af5a049b75c4f3d0357bfbc46f23622072a330a0a    14f502  0300  ffe0ed659d103d939dbbd6b7ac8fe8748c1fbc71b1f1f85bb91c8c44f09a8dc896  0b0030ef7dba02
-# 0x3d02    81  ffddd5ccff603c3801324a92fdb94d160d5efe5c460c491e3aed9117a1b340e69e  9452a34d4dd6289106706d058795404a4d9d5f25b8f5767a184e51ea63d7ed8c141632a9dc0653e4305fb1cca396829835e17e163b34520c4323f72c4f20890a    1c9501  0300  ffe0ed659d103d939dbbd6b7ac8fe8748c1fbc71b1f1f85bb91c8c44f09a8dc896  0b0030ef7dba02
-# 0300ff2c31ede147b6ba608a5f89bf218cb9962914895980b1881a4e4b7bd3a0faa2a60b0030ef7dba02001000dcd1346701ca8396496e52aa2785b1748deb6db09551b72159dcb3e08991025b
